d816fb85469b6c69f8a391f1f768cca51c43ca6c
Shi Yan <shiyan@lm-sjc-11000661.local>
2015-07-29 22:35:50 -0700

    Initial Commit

---
+++ SoHungry/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SoHungry
+//
+//  Created by Shi Yan on 7/29/15.
+//  Copyright © 2015 Shi Yan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
