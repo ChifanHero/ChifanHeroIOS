6b22e036f8b1f5aa1e96e17d25cb05ec219eb3d5
syan <family.syan@gmail.com>
2015-08-16 22:47:56 -0700

    Basic http client

--- SoHungry/HttpClient.swift
+++ SoHungry/HttpClient.swift
@@ -10,29 +10,61 @@ import Foundation
 
 class HttpClient {
     
-//    class func post(url:String, parameters : AnyObject, completionHandler: () -> Void, errorHandler:(error:ErrorType) -> Void) {
-//        let request = NSMutableURLRequest(URL:NSURL(string: url)!)
-//        let session = NSURLSession.sharedSession()
-//        request.HTTPMethod = "POST"
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue("application/json", forHTTPHeaderField: "Accept")
-//        do {
-//            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: NSJSONWritingOptions.PrettyPrinted)
-//        } catch {
-//            errorHandler(error: error)
-//        }
-//        let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
-//            //
-//        }
-//        NSJSONSerialization.JSONObjectWithData(nil, options: NSJSONReadingOptions.MutableContainers)
-//        task.resume()
-//        
-//        
-//        
-//        
-//    }
-//    
-//    class func get(url:String, parameters: AnyObject, completionHandler:()) {
-//        
-//    }
+    static let contentTypeHeader = "Content-Type"
+    static let acceptHeader = "Accept"
+    static let applicationJson = "application/json"
+    
+    var headers = [String : String]()
+    
+    init() {
+        setDefaultHeaders()
+    }
+    
+    func post(url:String, headers:[String : String]?, parameters : [String : AnyObject], completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        let request = NSMutableURLRequest(URL:NSURL(string: url)!)
+        request.HTTPMethod = "POST"
+        if headers != nil {
+            for (header, value) in headers! {
+                if self.headers[header] != value {
+                    self.headers[header] = value
+                }
+            }
+        }
+        for (header, value) in self.headers {
+            request.addValue(value, forHTTPHeaderField: header)
+        }
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: NSJSONWritingOptions.PrettyPrinted)
+        } catch {
+            print(error)
+        }
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+            completionHandler(data, response, error)
+        }
+        task.resume()
+    }
+    
+    func get(url:String, headers:[String : String]?, parameters : [String : AnyObject], completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) {
+        let request = NSMutableURLRequest(URL:NSURL(string: url)!)
+        request.HTTPMethod = "GET"
+        if headers != nil {
+            for (header, value) in headers! {
+                if self.headers[header] != value {
+                    self.headers[header] = value
+                }
+            }
+        }
+        for (header, value) in self.headers {
+            request.addValue(value, forHTTPHeaderField: header)
+        }
+        let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!) { (data, response, error) -> Void in
+            completionHandler(data, response, error)
+        }
+        task.resume()
+    }
+    
+    private func setDefaultHeaders() {
+        headers[HttpClient.contentTypeHeader] = HttpClient.applicationJson
+        headers[HttpClient.acceptHeader] = HttpClient.applicationJson
+    }
 }
