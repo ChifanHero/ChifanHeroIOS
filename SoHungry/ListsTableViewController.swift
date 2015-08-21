bf6ffd1d5fca09914ffbe0be675723689ea5c31a
syan <family.syan@gmail.com>
2015-08-21 00:00:19 -0700

    lists view init

---
+++ SoHungry/ListsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListsTableViewController.swift
+//  SoHungry
+//
+//  Created by Shi Yan on 8/20/15.
+//  Copyright © 2015 Shi Yan. All rights reserved.
+//
+
+import UIKit
+
+class ListsTableViewController: UITableViewController {
+
+}
