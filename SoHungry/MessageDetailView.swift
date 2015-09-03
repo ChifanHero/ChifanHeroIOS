ae9e154d8e700d185aafe38e76becb3715203368
syan <family.syan@gmail.com>
2015-09-02 17:17:35 -0700

    message detail view init

---
+++ SoHungry/MessageDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  MessageDetailView.swift
+//  SoHungry
+//
+//  Created by Shi Yan on 9/2/15.
+//  Copyright © 2015 Shi Yan. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
