f886d866ded894ad322e9b654d8e6771027a82ce
syan <family.syan@gmail.com>
2015-09-04 18:22:18 -0700

    more about search view

--- SoHungry/SearchViewController.swift
+++ SoHungry/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate, SelectionPanelDelegate{
 
     @IBOutlet weak var selectionPanel: SelectionPanel!
     
@@ -16,13 +16,28 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Search view");
-        self.navigationItem.titleView = UISearchBar()
+        let searchBar : UISearchBar = UISearchBar()
+        searchBar.placeholder = "大家都在搜：韶山冲"
+        searchBar.delegate = self
+        self.navigationItem.titleView = searchBar
         selectionPanel.setUpSelectionPanel(options: ["餐厅", "菜名", "榜单"], defaultSelection: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.placeholder = "AAAAA"
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchBar.text)
+    }
+    
+    func selectionPanel(selectionPanel : SelectionPanel, didSelectElementAtIndex : Int) -> Void {
+        
     }
 
 
--- SoHungry/SelectionPanelDelegateProtocol.swift
+++ SoHungry/SelectionPanelDelegateProtocol.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-protocol SelectionPanelDelegateProtocol {
+protocol SelectionPanelDelegate {
     
     func selectionPanel(selectionPanel : SelectionPanel, didSelectElementAtIndex : Int) -> Void
     
