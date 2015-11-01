0236645b55ffe7f8bc3ec45e2b4d3c22442b477d
syan <family.syan@gmail.com>
2015-11-01 09:47:31 -0800

    search init

--- SoHungry/SearchViewController.swift
+++ SoHungry/SearchViewController.swift
@@ -8,21 +8,42 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UISearchBarDelegate{
+class SearchViewController: UIViewController, UISearchResultsUpdating, UISearchControllerDelegate, ImageProgressiveTableViewDelegate{
     
     @IBOutlet weak var selectionBar: SelectionBar!
     
-    var searchBar : UISearchBar?
+    
+    @IBOutlet weak var searchResultTableView: UITableView!
+    
+    
+    var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Search view");
-        searchBar = UISearchBar()
-        searchBar!.barTintColor = UIColor.clearColor()
-        searchBar!.placeholder = "大家都在搜：韶山冲"
-        searchBar!.delegate = self
-        self.navigationItem.titleView = searchBar
+        
+        searchController = UISearchController(searchResultsController: nil)
+        
+        // The object responsible for updating the contents of the search results controller.
+        searchController.searchResultsUpdater = self
+        
+        // Determines whether the underlying content is dimmed during a search.
+        // if we are presenting the display results in the same view, this should be false
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        searchController.delegate = self
+        
+        // Make sure the that the search bar is visible within the navigation bar.
+        searchController.searchBar.sizeToFit()
+        self.navigationItem.titleView = searchController.searchBar
+        searchController.searchBar.barTintColor = UIColor.clearColor()
+        searchController.searchBar.placeholder = "大家都在搜：韶山冲"
+
+        definesPresentationContext = true
+        searchResultTableView.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,19 +51,27 @@ class SearchViewController: UIViewController, UISearchBarDelegate{
         // Dispose of any resources that can be recreated.
     }
     
-    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-        searchBar.placeholder = "AAAAA"
-        self.searchBar?.setShowsCancelButton(true, animated: true)
-    }
-    
-    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-        self.searchBar?.endEditing(true)
-        self.searchBar?.setShowsCancelButton(false, animated: true)
-    }
+//    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+//        searchBar.placeholder = "AAAAA"
+//        self.searchBar?.setShowsCancelButton(true, animated: true)
+//    }
+//    
+//    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+//        self.searchBar?.endEditing(true)
+//        self.searchBar?.setShowsCancelButton(false, animated: true)
+//    }
     
     
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchBar.text)
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+    }
+    
+    func imageForIndexPath(tableView tableView : UITableView, indexPath : NSIndexPath) -> PhotoRecord {
+        return PhotoRecord(name: "", url: NSURL())
     }
     
 }
