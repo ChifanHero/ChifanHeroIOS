0f307e3db1c0cbae7d59c42eafad86f9626c4d95
syan <family.syan@gmail.com>
2015-12-03 23:13:52 -0800

    migrate search code to new project

---
+++ Lightning/CustomUIRefreshControl.swift
@@ -0,0 +1,21 @@
+//
+//  CustomUIRefreshControl.swift
+//  SoHungry
+//
+//  Created by Shi Yan on 10/31/15.
+//  Copyright © 2015 Shi Yan. All rights reserved.
+//
+
+import UIKit
+
+class CustomUIRefreshControl: UIRefreshControl {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
