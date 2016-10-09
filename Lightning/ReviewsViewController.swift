8634f0eb13422b6ad902593869e94cfde66185f2
family.syan <family.syan@gmail.com>
2016-10-09 13:28:16 -0700

    reviews view basic layout

--- Lightning/ReviewsViewController.swift
+++ Lightning/ReviewsViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 import BTNavigationDropdownMenu
 
 class ReviewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var reviewsTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configDropDownMenu()
         addBarButton()
         self.addImageForBackBarButtonItem()
+        self.clearTitleForBackBarButtonItem()
         // Do any additional setup after loading the view.
     }
 
@@ -45,12 +49,51 @@ class ReviewsViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var cell: ReviewSnapshotTableViewCell? = tableView.dequeueReusableCellWithIdentifier("reviewSnapshotCell") as? ReviewSnapshotTableViewCell
+        if cell == nil {
+            tableView.registerNib(UINib(nibName: "ReviewSnapshotCell", bundle: nil), forCellReuseIdentifier: "reviewSnapshotCell")
+            cell = tableView.dequeueReusableCellWithIdentifier("reviewSnapshotCell") as? ReviewSnapshotTableViewCell
+        }
+        if indexPath.section == 2 {
+            cell?.userName = "Peter Huang"
+            cell?.profileImageView.image = UIImage(named: "peter")
+            cell?.review = "Who tm cares?"
+        }
+        return cell!
     }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 166
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView()
+        footerView.backgroundColor = UIColor.groupTableViewBackgroundColor()
+        return footerView
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell : UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
+        showReview()
+        cell.selected = false
+    }
+    
+    func showReview() {
+        performSegueWithIdentifier("showReview", sender: nil)
+    }
+    
     
 
     /*
